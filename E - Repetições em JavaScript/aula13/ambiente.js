d6c6dcd9205d04e6d86b07e174c402ff2eb43c7a
DevRibeiro09 <contatodevribeiro@gmail.com>
2024-03-11 16:24:04 -0300

    Repetições

---
+++ E - Repetições em JavaScript/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c == c + 1
+} while (c <= 6)
+
+// OS DOIS SÃO BASICAMENTE A MESMA COISA, VAI DEPENDER DA SITUAÇÃO PARA UTILIZAR CADA UM
+
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c == c + 1
+}
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ E - Repetições em JavaScript/aula14/ex16/script.js
@@ -0,0 +1,6 @@
+function contar() {
+    var inicio = window.document.getElementById("inicio")
+    var fim = document.getElementById("fim")
+    var passo = document.getElementById("passo")
+
+}
